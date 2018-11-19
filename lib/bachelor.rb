e463008f67477b1f96aef0af4d9bd8b3fbcf0058
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-19 18:27:29 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,66 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_first_name = []
+  data.each do |season_number, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if value == "Winner" && season_number == season
+          winner_first_name = contestant["name"].split
+        end 
+      end 
+    end 
+  end 
+  winner_first_name[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_number, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if value == occupation
+          return contestant["name"]
+        end 
+      end 
+    end 
+  end 
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  number_from = 0
+  data.each do |season_number, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if value == hometown
+          number_from += 1
+        end 
+      end 
+    end 
+  end 
+  number_from
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+          if hometown == value 
+            return contestant["occupation"]
+        end 
+      end 
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  average_array = []
+  data.each do |season_number, contestants|
+    contestants.each do |contestant|
+      contestant.each do |attribute, value|
+        if season_number == season
+            average_array << contestant["age"].to_i
+        end 
+      end 
+    end 
+  end 
+  (average_array.inject{|x, y| x + y}.to_f / average_array.length).round
 end
